746cf6148f15e2f4b70329e69f78976b6ec65480
Faisal Bagalagel <uultradude@gmail.com>
2022-08-15 12:18:04 -0400

    docs: add more details to examples

--- examples/stake_cpu.py
+++ examples/stake_cpu.py
@@ -1,21 +1,22 @@
-"""Take cpu to a account."""
+"""Stake cpu / delegate bandwitdh to an account."""
 
 import eospyo
 
 data = [
+    # In this case the account me.wam is staking cpu for itself
     eospyo.Data(name="from", value=eospyo.types.Name("me.wam")),
     eospyo.Data(name="receiver", value=eospyo.types.Name("me.wam")),
     eospyo.Data(
-        name="stake_cpu_quantity",
-        value=eospyo.types.Asset("15.00000000 WAX"),
+        name="stake_cpu_quantity", # Selects the 'stake_cpu_quantity' field in this action to stake cpu, any fields must exist in the action to be selected
+        value=eospyo.types.Asset("15.00000000 WAX"), # Asset type must be specified as 'stake_cpu_quantity' requires the amount and currency type, which Asset includes
     ),
     eospyo.Data(
-        name="stake_net_quantity",
-        value=eospyo.types.Asset("30.00000000 WAX"),
+        name="stake_net_quantity", # Selects the 'stake_net_quantity' field in this action to stake net
+        value=eospyo.types.Asset("30.00000000 WAX"), # Asset type must be specified as 'stake_net_quantity' requires the amount and currency type, which Asset includes
     ),
     eospyo.Data(
-        name="transfer",
-        value=eospyo.types.Bool(False),
+        name="transfer", # Selects the 'transfer' field in this action to stake cpu
+        value=eospyo.types.Bool(False), # Bool type used to indicate transfer
     ),
 ]
 
--- examples/transfer.py
+++ examples/transfer.py
@@ -3,15 +3,16 @@
 import eospyo
 
 data = [
+    # In this case the account me.wam is transferring to account 'reciever'
     eospyo.Data(name="from", value=eospyo.types.Name("me.wam")),
     eospyo.Data(name="to", value=eospyo.types.Name("receiver")),
     eospyo.Data(
-        name="quantity",
-        value=eospyo.types.Asset("55.00000000 WAX"),
+        name="quantity", # Selects the 'quantity' field in this action, must be a valid field in the action
+        value=eospyo.types.Asset("55.00000000 WAX"), # Asset type must be specified as 'quantity' requires the amount and currency type, which Asset includes
     ),
     eospyo.Data(
-        name="memo",
-        value=eospyo.types.String("Trying EosPyo"),
+        name="memo", # Selects the 'memo' field in this action, just an extra message with the transfer
+        value=eospyo.types.String("Trying EosPyo"), # String type is used for memo
     ),
 ]
 
