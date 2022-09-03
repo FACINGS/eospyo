83b334e968a48608a6a663382565f82bddeeb303
Edinho <git@final04.com>
2022-09-03 13:14:12 -0300

    feat: Change hashlib to pycryptodome when RIPEMD160 isn't available
    - newer SSL builds doesn't support RIPEMD160 algorithms
    - whenever we can't use it systemwide, will revert to pycryptodome
    implementation instead
    - vendored `ripmed160` and `unpack_signature` from ueosio (ds.py, L12
    and L464)

--- eospyo/transaction.py
+++ eospyo/transaction.py
@@ -1,6 +1,7 @@
 """Transaction, Authorization and Action classes."""
 
 
+import base58
 import datetime as dt
 import hashlib
 import json
@@ -332,9 +333,31 @@ def sign_bytes(bytes_: bytes, key: str) -> str:
         nonce += 1
 
     ds = ueosio.DataStream(signature)
-    signature = ds.unpack_signature()
+    signature = _unpack_signature(data_stream=ds)
 
     return signature
+
+
+def _ripmed160(data):
+    try:
+        h = hashlib.new("ripemd160")
+    except ValueError:
+        from Crypto.Hash import RIPEMD160
+        h = RIPEMD160.new()
+    h.update(data)
+    return h.digest()
+
+
+def _unpack_signature(data_stream) -> str:
+    t = data_stream.unpack_uint8()
+    if t == 0:
+        data = data_stream.read(65)
+        data = data + _ripmed160(data + b"K1")[:4]
+        return "SIG_K1_" + base58.b58encode(data).decode("ascii")
+    elif t == 1:
+        raise NotImplementedError("Not Implemented")
+    else:
+        raise ValueError("Invalid binary signature")
 
 
 class SignedTransaction(LinkedTransaction):
